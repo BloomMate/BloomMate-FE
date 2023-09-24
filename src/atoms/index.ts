7e13a6485526cfc6e85f50f7112a49eaea1f6010
DongHyunKim <akainoo123@gmail.com>
2023-09-24 18:30:42 +0900

    fix : fix the text-input naming

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,6 +1,6 @@
 export * from './text';
 export * from './icon';
 export * from './button';
-export * from './textinput';
+export * from './text-input';
 export * from './modal';
 export * from './divider';
