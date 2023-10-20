a571836d2289fb9f31dbf64adb7a81e3be43c283
hyuna <hyunah0923@hanyang.ac.kr>
2023-10-20 20:42:20 +0900

    feat : signup-screen

--- src/screens/signup/modules/signup-footer/signup-footer.module.tsx
+++ src/screens/signup/modules/signup-footer/signup-footer.module.tsx
@@ -1,11 +1,20 @@
 import { Box } from '@mobily/stacks';
 import { memo } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { $signUpState, signUpSteps } from '../../signup.state';
 
 import { Button } from '@/atoms';
 
 type SignUpFooterModuleProps = {};
 
 export const SignUpFooterModule = memo<SignUpFooterModuleProps>(() => {
+  const [{ screenStep }, setSignUpState] = useRecoilState($signUpState);
+  const currentScreenStepIndex = signUpSteps.findIndex(screenStep);
+
+  const copy =
+    currentScreenStepIndex === signUpSteps.length() - 1 ? '회원가입' : '다음';
+
   return (
     <Box>
       <Button mode="contained">회원가입</Button>
