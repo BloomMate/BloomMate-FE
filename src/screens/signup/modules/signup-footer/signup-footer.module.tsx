835534916aef2e4a3425cb8fb29d2aeecb8da048
hyuna <hyunah0923@hanyang.ac.kr>
2023-10-27 03:08:19 +0900

    feat: 회원가입 구현 (세부적인 퍼블리싱 제외 돌아가게만!!)

--- src/screens/signup/modules/signup-footer/signup-footer.module.tsx
+++ src/screens/signup/modules/signup-footer/signup-footer.module.tsx
@@ -1,28 +1,51 @@
 import { Box } from '@mobily/stacks';
+import { useNavigation } from '@react-navigation/native';
 import { memo } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { SubmitHandler, useFormContext } from 'react-hook-form';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { SignUpForm } from '../../hooks';
+import { SignUpScreenNavigationProps } from '../../signup.screen';
 import {
   $currentScreenStepIndexSelector,
   $signUpState,
   signUpSteps,
 } from '../../signup.state';
 
+import { usePostSignupMutation } from './hooks';
+
 import { Button } from '@/atoms';
+import { useMutationIndicator } from '@/providers';
 
 type SignUpFooterModuleProps = {};
 
 export const SignUpFooterModule = memo<SignUpFooterModuleProps>(() => {
+  const navigation = useNavigation<SignUpScreenNavigationProps>();
   const setSignUpState = useSetRecoilState($signUpState);
 
   const currentScreenStepIndex = useRecoilValue(
     $currentScreenStepIndexSelector,
   );
+  const { mutateAsync, isLoading } = usePostSignupMutation();
+
+  useMutationIndicator([isLoading]);
 
   const { formState, handleSubmit } = useFormContext<SignUpForm>();
+
   const { isDirty, isValid, errors, dirtyFields } = formState;
+
+  const addNewPerson: SubmitHandler<SignUpForm> = async ({
+    ID_INPUT: id,
+    PW_INPUT: pw,
+    NAME_INPUT: name,
+    TIIUN_INPUT: tiiun,
+    TURF_INPUT: gardensize,
+    ADDRESS_INPUT: address,
+  }) => {
+    await mutateAsync({ id, pw, name, tiiun, gardensize, address });
+    navigation.navigate('LoginScreen');
+  };
+
   const isSignUpPossible = isDirty && isValid;
   const isCurrentStepValid = () => {
     const fieldName = signUpSteps[currentScreenStepIndex];
@@ -35,7 +58,11 @@ export const SignUpFooterModule = memo<SignUpFooterModuleProps>(() => {
   const copy = isLastStep ? '회원가입' : '다음';
 
   const handlePressButton = () => {
-    setSignUpState({ screenStep: signUpSteps[currentScreenStepIndex + 1] });
+    if (isLastStep) {
+      handleSubmit(addNewPerson)();
+    } else {
+      setSignUpState({ screenStep: signUpSteps[currentScreenStepIndex + 1] });
+    }
   };
 
   return (
