ac95ddce1c90e937f8761360e755ff4ac9337337
hyuna <hyunah0923@hanyang.ac.kr>
2023-10-27 16:32:26 +0900

    feat: 정렬

--- src/screens/landing/landing.screen.tsx
+++ src/screens/landing/landing.screen.tsx
@@ -1,9 +1,11 @@
 import { Row, Rows, Stack } from '@mobily/stacks';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Image } from 'react-native';
 
 import { RootStackParamList } from '../root.navigator';
 
+import { bgImageURL, logoImageURL } from './const';
 import {
   LandingLoginModule,
   LandingSignUpModule,
@@ -26,10 +28,20 @@ export type LandingScreenNavigationRouteProps = RouteProp<
 export const LandingScreen = ({}: LandingScreenProps) => {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <Image
+        source={bgImageURL}
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+        }}
+      />
       <BasicLayout>
         <Rows>
-          <Row height="fluid">
-            <Stack space={32}>
+          <Row height="fluid" style={{ justifyContent: 'center' }}>
+            <Stack space={80} align={'center'}>
+              <Image source={logoImageURL} style={{ width: 290, height: 56 }} />
               <LandingTextModule />
             </Stack>
           </Row>
