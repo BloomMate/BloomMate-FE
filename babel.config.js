f39ae1a7384caef1c2db059c11a53ef1b32c199a
DongHyunKim <akainoo123@gmail.com>
2023-11-06 15:26:21 +0900

    feat : .env 파일 생성

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,19 @@ module.exports = {
   },
   plugins: [
     [
+			'module:react-native-dotenv',
+			{
+				"envName": "APP_ENV",
+				"moduleName": "@env",
+				"path": ".env",
+				"blocklist": null,
+				"allowlist": null,
+				"safe": false,
+				"allowUndefined": false,
+				"verbose": false
+			},
+		],
+    [
       'module-resolver',
       {
         root: ['./src'],
