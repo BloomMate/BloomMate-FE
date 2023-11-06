bcd030a501e100ad333a02d98f5348f73886a2c5
DongHyunKim <akainoo123@gmail.com>
2023-11-06 20:40:21 +0900

    feat : button icon 처리

--- src/atoms/button/button.atom.tsx
+++ src/atoms/button/button.atom.tsx
@@ -1,19 +1,28 @@
+import { Stack } from '@mobily/stacks';
 import color from 'color';
+import isUndefined from 'lodash/isUndefined';
 import { ComponentProps } from 'react';
-import { Button as PaperButton } from 'react-native-paper';
+import { Button as PaperButton, TouchableRipple } from 'react-native-paper';
 
-import { getVariantsStyle } from '../text';
+import { Icon } from '../icon';
+import { Text, getVariantsStyle } from '../text';
 
 import { palette } from '@/utils';
 
-export type ButtonProps = Omit<ComponentProps<typeof PaperButton>, 'mode'> & {
+export type ButtonProps = Omit<
+  ComponentProps<typeof PaperButton>,
+  'mode' | 'icon'
+> & {
   mode: 'outlined' | 'contained' | 'text';
+  icon: string;
 };
 
 export const Button = ({
   children,
   mode,
   contentStyle,
+  icon,
+  onPress,
   ...props
 }: ButtonProps) => {
   const textColor =
@@ -22,9 +31,32 @@ export const Button = ({
   const labelLargeFontStyle = getVariantsStyle('labelLarge');
   const rippleColor = color(textColor).alpha(0.33).rgb().string();
 
+  if (!isUndefined(icon)) {
+    return (
+      <TouchableRipple
+        rippleColor={rippleColor}
+        style={{
+          backgroundColor: palette['primary'],
+          height: 48,
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 8,
+        }}
+        onPress={onPress}>
+        <Stack space={8} horizontal align="center">
+          <Text fontWeight="Medium" variants="labelLarge" color="white">
+            {children}
+          </Text>
+          <Icon name={icon} size={20} color={palette['white']} />
+        </Stack>
+      </TouchableRipple>
+    );
+  }
+
   return (
     <PaperButton
       mode={mode}
+      onPress={onPress}
       rippleColor={rippleColor}
       contentStyle={[{ height: 48 }, contentStyle]}
       theme={{
--- src/screens/primary/primary-plant-list/modules/primary-plant-current-list/primary-plant-current-list.module.tsx
+++ src/screens/primary/primary-plant-list/modules/primary-plant-current-list/primary-plant-current-list.module.tsx
@@ -21,8 +21,10 @@ export const PrimaryPlantCurrentListModule =
             textAlignment="center">
             {'등록된 식물이 없어요.\n첫 식물을 등록해주세요!'}
           </Text>
-          <Button mode="contained" onPress={() => {}}>
-            식물 등록
+          <Button mode="contained" onPress={() => {}} icon="add">
+            <Text variants="labelLarge" fontWeight="Medium" color="white">
+              식물 등록
+            </Text>
           </Button>
         </Stack>
       </Box>
