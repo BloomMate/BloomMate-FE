a9280625dd9ca89ecdeab93a49b3830478bb40a8
DongHyunKim <akainoo123@gmail.com>
2023-11-07 21:13:16 +0900

    feat : article-list-dummy-data

--- src/dummy-data/index.ts
+++ src/dummy-data/index.ts
@@ -1,3 +1,5 @@
-export * from './plant-list-dummy-data';
+export * from './article-list-dummy-data';
 export * from './plant-detail-dummy-data';
+export * from './plant-list-dummy-data';
+export * from './question-detail-dummy-data';
 export * from './question-list-dummy-data';
