4e1d0f624f28fb5720b5047c4523b3a56d76f7b9
DongHyunKim <akainoo123@gmail.com>
2023-11-07 16:04:35 +0900

    feat : add handlePress

--- src/screens/plant-detail/plant-detail.screen.tsx
+++ src/screens/plant-detail/plant-detail.screen.tsx
@@ -1,9 +1,10 @@
-import { RouteProp, useNavigation } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { RootStackParamList } from '../root.navigator';
 
-import { BasicLayout, ModalHeader } from '@/layouts';
+import { Button } from '@/atoms';
+import { BasicLayout, ModalHeader, ScrollView } from '@/layouts';
 
 type PlantDetailScreenProps = {};
 
@@ -18,14 +19,29 @@ export type PlantDetailScreenNavigationRouteProps = RouteProp<
 >;
 
 export const PlantDetailScreen = ({}: PlantDetailScreenProps) => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<PlantDetailScreenNavigationProps>();
+  const {
+    params: { id },
+  } = useRoute<PlantDetailScreenNavigationRouteProps>();
+
+  const handlePressDiagnosisButton = () => {
+    navigation.navigate('PlantDiagnosisIntroScreen', { id });
+  };
 
   return (
-    <BasicLayout backgroundColor="gray-100">
-      <ModalHeader
-        left={{ type: 'icon' }}
-        onPressExit={() => navigation.goBack()}
-      />
-    </BasicLayout>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <BasicLayout backgroundColor="gray-100">
+        <ModalHeader
+          left={{ type: 'icon' }}
+          onPressExit={() => navigation.goBack()}
+        />
+      </BasicLayout>
+      <Button
+        mode="contained"
+        icon="health-and-safety"
+        onPress={handlePressDiagnosisButton}>
+        AI 진단하기
+      </Button>
+    </ScrollView>
   );
 };
