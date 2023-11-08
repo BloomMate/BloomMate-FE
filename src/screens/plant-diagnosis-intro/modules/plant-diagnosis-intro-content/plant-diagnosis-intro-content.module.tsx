cd536c2b60b1578c3d573f0fe76102580fad3e27
DongHyunKim <akainoo123@gmail.com>
2023-11-08 19:03:42 +0900

    feat : plant-diagnosis-intro 완료

--- src/screens/plant-diagnosis-intro/modules/plant-diagnosis-intro-content/plant-diagnosis-intro-content.module.tsx
+++ src/screens/plant-diagnosis-intro/modules/plant-diagnosis-intro-content/plant-diagnosis-intro-content.module.tsx
@@ -1,9 +1,61 @@
-import { Box } from '@mobily/stacks';
+import { Box, Stack } from '@mobily/stacks';
 import { memo } from 'react';
+import { Image } from 'react-native';
+
+import { Icon, Text } from '@/atoms';
+import { palette } from '@/utils';
 
 type PlantDiagnosisIntroContentModuleProps = {};
 
 export const PlantDiagnosisIntroContentModule =
   memo<PlantDiagnosisIntroContentModuleProps>(() => {
-    return <Box flex="fluid" />;
+    return (
+      <Box flex="fluid" paddingTop={48} alignX="center">
+        <Stack space={64}>
+          <Stack space={36} align="center">
+            <Box>
+              <Image
+                source={require('./assets/plant-diagnosis.png')}
+                style={{ width: 200, height: 200, borderRadius: 100 }}
+                resizeMode="contain"
+              />
+              <Icon
+                style={{ position: 'absolute', bottom: 0, right: 0 }}
+                name="search-off"
+                size={48}
+                color={palette['gray-900']}
+              />
+            </Box>
+            <Stack space={8}>
+              <Text variants="titleLarge" fontWeight="Medium" color="gray-900">
+                AI 식물 진단 - 팝콘묵자
+              </Text>
+              <Text variants="labelLarge" fontWeight="Medium" color="gray-700">
+                본 진단은 LG 스마트 코티지 야외용 틔운 텃밭의 식물을 촬영하여 AI
+                러닝을 기반으로 병해를 진단하고, 관리법을 추천드리는
+                서비스입니다.
+              </Text>
+            </Stack>
+          </Stack>
+          <Box
+            paddingY={16}
+            alignX="center"
+            style={{
+              backgroundColor: palette['white'],
+              borderRadius: 8,
+              elevation: 3,
+            }}>
+            <Text
+              variants="bodyLarge"
+              fontWeight="Medium"
+              color="primary"
+              textAlignment="center">
+              {
+                '주기적으로 스마트 코티지에\n방문하셔서 식물을 진단하고\n관리해주셔야 식물이 잘 자랄거에요!'
+              }
+            </Text>
+          </Box>
+        </Stack>
+      </Box>
+    );
   });
