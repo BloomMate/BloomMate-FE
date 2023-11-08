777f051b8da8f1bece86ad490c175870a591963f
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2023-11-08 20:50:04 +0900

    feat/detail 모듈화 contents

--- src/screens/plant-detail/modules/plant-details-content/plant-details-content.module.tsx
+++ src/screens/plant-detail/modules/plant-details-content/plant-details-content.module.tsx
@@ -1,18 +1,25 @@
-import { memo } from "react";
-import { PlantDetailHeaderModule } from "../plant-detail-header";
-import { PlantDetailDetailsModule } from "./plant-detail-details/plant-detail-details";
-import { PlantDetailPictureModule } from "./plant-detail-picture";
-import { PlantDetailGrowthModule } from "./plant-details-growth";
-import { PlantDetailScreenNavigationProps } from "../../plant-detail.screen";
-import { useNavigation } from "@react-navigation/native";
+import { Stack } from '@mobily/stacks';
+import { useNavigation } from '@react-navigation/native';
+import { memo } from 'react';
+
+import { PlantDetailScreenNavigationProps } from '../../plant-detail.screen';
+import { PlantDetailHeaderModule } from '../plant-detail-header';
+
+import { PlantDetailDetailsModule } from './plant-detail-details/plant-detail-details';
+import { PlantDetailPictureModule } from './plant-detail-picture';
+import { PlantDetailGrowthModule } from './plant-details-growth';
+
 type PlantDetailContentModule = {};
 
 export const PlantDetailContentModule = memo<PlantDetailContentModule>(() => {
-    const navigation = useNavigation<PlantDetailScreenNavigationProps>();
-    return (
-        <PlantDetailHeaderModule/>
-        <PlantDetailPictureModule/>
-        <PlantDetailDetailsModule/>
-        <PlantDetailGrowthModule/>
-    );    
-}) 
+  const navigation = useNavigation<PlantDetailScreenNavigationProps>();
+
+  return (
+    <Stack>
+      <PlantDetailHeaderModule />
+      <PlantDetailPictureModule />
+      <PlantDetailDetailsModule />
+      <PlantDetailGrowthModule />
+    </Stack>
+  );
+});
