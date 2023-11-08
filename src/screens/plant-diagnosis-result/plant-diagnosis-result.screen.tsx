4a0c9a81c495ea98e1f35bb861e73f8637305099
DongHyunKim <akainoo123@gmail.com>
2023-11-08 19:54:40 +0900

    feat : picture library

--- src/screens/plant-diagnosis-result/plant-diagnosis-result.screen.tsx
+++ src/screens/plant-diagnosis-result/plant-diagnosis-result.screen.tsx
@@ -1,7 +1,10 @@
-import { RouteProp } from '@react-navigation/native';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Image } from 'react-native';
 
 import { RootStackParamList } from '../root.navigator';
+
+import { BasicLayout } from '@/layouts';
 
 type PlantDiagnosisResultScreenProps = {};
 
@@ -17,5 +20,16 @@ export type PlantDiagnosisResultScreenNavigationRouteProps = RouteProp<
 
 export const PlantDiagnosisResultScreen =
   ({}: PlantDiagnosisResultScreenProps) => {
-    return null;
+    const {
+      params: { photo_url },
+    } = useRoute<PlantDiagnosisResultScreenNavigationRouteProps>();
+
+    return (
+      <BasicLayout backgroundColor="gray-100">
+        <Image
+          source={{ uri: photo_url }}
+          style={{ width: 100, height: 100 }}
+        />
+      </BasicLayout>
+    );
   };
