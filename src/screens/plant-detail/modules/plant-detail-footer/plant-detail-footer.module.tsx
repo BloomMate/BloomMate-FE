8abc3ff25fea292e8459f69a32f445d20865216e
DongHyunKim <akainoo123@gmail.com>
2023-11-11 12:39:49 +0900

    feat : plant-detail-footer 완료

--- src/screens/plant-detail/modules/plant-detail-footer/plant-detail-footer.module.tsx
+++ src/screens/plant-detail/modules/plant-detail-footer/plant-detail-footer.module.tsx
@@ -1,28 +1,49 @@
 import { Stack } from '@mobily/stacks';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import isUndefined from 'lodash/isUndefined';
 import { memo } from 'react';
 
-import { PlantDetailScreenNavigationProps } from '../../plant-detail.screen';
+import {
+  PlantDetailScreenNavigationProps,
+  PlantDetailScreenNavigationRouteProps,
+} from '../../plant-detail.screen';
 
 import { Button } from '@/atoms';
+import { useGetPlantDetailQuery } from '@/hooks';
 
 type PlantDetailFooterModule = {};
 
 export const PlantDetailFooterModule = memo<PlantDetailFooterModule>(() => {
   const navigation = useNavigation<PlantDetailScreenNavigationProps>();
+  const {
+    params: { id },
+  } = useRoute<PlantDetailScreenNavigationRouteProps>();
 
-  const handlePressDiagnosisButton = () => {
-    // navigation.navigate('PlantDiagnosisIntroScreen', { id });
-    console.log('button clicked');
+  const { data } = useGetPlantDetailQuery({ plant_id: id });
+
+  if (isUndefined(data)) {
+    return;
+  }
+  const { growth_level } = data;
+
+  const isGrowthLevelHarvest = growth_level === 'harvest';
+
+  const handlePressButton = () => {
+    if (isGrowthLevelHarvest) {
+      // TODO : ADD Harvest API + navigate to list
+
+      return;
+    }
+    navigation.navigate('PlantDiagnosisIntroScreen', { id });
   };
 
   return (
     <Stack space={32}>
       <Button
         mode="contained"
-        icon="health-and-safety"
-        onPress={handlePressDiagnosisButton}>
-        AI 진단하기
+        icon={isGrowthLevelHarvest ? 'agriculture' : 'health-and-safety'}
+        onPress={handlePressButton}>
+        {isGrowthLevelHarvest ? '수확하기' : 'AI 진단하기'}
       </Button>
     </Stack>
   );
