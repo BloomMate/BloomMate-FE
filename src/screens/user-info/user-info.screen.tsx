2e7a0255e56bfd04477756c62242ccb44822a37e
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2023-11-15 13:35:43 +0900

    userinfo dummy

--- src/screens/user-info/user-info.screen.tsx
+++ src/screens/user-info/user-info.screen.tsx
@@ -1,10 +1,11 @@
 import { Stack } from '@mobily/stacks';
-import { RouteProp, useNavigation } from '@react-navigation/native';
+import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { RootStackParamList } from '../root.navigator';
 
 import { Text } from '@/atoms';
+import { USER_DETAIL_DUMMY_DATA } from '@/dummy-data/user-detail-dummy-data';
 import { BasicLayout } from '@/layouts';
 
 type UserInfoScreenProps = {};
@@ -20,15 +21,30 @@ export type UserInfoScreenNavigationRouteProps = RouteProp<
 >;
 
 export const UserInfoScreen = ({}: UserInfoScreenProps) => {
-  const navigation = useNavigation<UserInfoScreenNavigationProps>();
+  const data = USER_DETAIL_DUMMY_DATA; // 더미 데이터를 직접 사용합니다
+
+  const { user_name, account_id, tiiun_number, garden_size, address } = data;
 
   return (
     <BasicLayout backgroundColor="gray-100">
       <Stack>
-        <Text variants={'displayLarge'} fontWeight={'Light'} color={'gray-900'}>
-          안녕하세요
-        </Text>
+        <UserInfoItem title="이름" content={user_name} />
+        <UserInfoItem title="계정 ID" content={account_id} />
+        <UserInfoItem title="Tiiun 번호" content={tiiun_number} />
+        <UserInfoItem title="정원 크기" content={String(garden_size)} />
+        <UserInfoItem title="주소" content={address} />
       </Stack>
     </BasicLayout>
   );
 };
+
+interface UserInfoItemProps {
+  title: string;
+  content: string;
+}
+
+const UserInfoItem: React.FC<UserInfoItemProps> = ({ title, content }) => (
+  <Text variants={'bodyMedium'} fontWeight={'Medium'} color={'gray-900'}>
+    {title}: {content}
+  </Text>
+);
