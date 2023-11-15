eb8fcc987ecf20de4c0bee49b70bdf149f2ae809
hyuna <hyunah0923@hanyang.ac.kr>
2023-11-15 14:29:34 +0900

    feat: 노션 연결

--- src/screens/primary/primary-community/screen/primary-community-article/modules/primary-community-article-content.const.ts
+++ src/screens/primary/primary-community/screen/primary-community-article/modules/primary-community-article-content.const.ts
@@ -7,56 +7,64 @@ export const PRIMARY_COMMUNITY_ARTICLE_DATA: {
   {
     id: '1',
     article_title: 'EP01 초보 식집사님을 위한, 농사 기초 용어 모음',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP01-27d28761743545f0900da59052e991a0?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585038/001_poybv6.png',
   },
   {
     id: '2',
     article_title: 'EP02 감자에 독이 있다고?! 😫',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP02-8caf4289c8504aa9816826abdb1b16d4?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585039/002_icfaze.png',
   },
   {
     id: '3',
     article_title: 'EP03 옥수수수염까지 활용하기 🌽',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP03-dede5d7fd7fb4d299d3808c83ce16a54?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585040/003_ikb5ux.png',
   },
   {
     id: '4',
     article_title: 'EP04 딸기, 월동 준비 ❄️',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP04-9113163824a44abdbf7606d17bef65b2?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585039/004_ofko46.png',
   },
   {
     id: '5',
     article_title: 'EP05 토마토는 다 같은 토마토? 🍅',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP05-dee4440ba436419f90648f125572d100?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585041/005_dv12mz.png',
   },
   {
     id: '6',
     article_title: 'EP06 감자 오래오래 보관하는 법 🥔',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP06-0dde6afb148a466d8eaccc2fa915a74a?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585040/006_kaf49x.png',
   },
   {
     id: '7',
     article_title: 'EP07 옥수수를 튀기면 팝콘이 된다?',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP07-91455cfade234f6d80a26ec3838e2fcd?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585041/007_vdtnyv.png',
   },
   {
     id: '8',
     article_title: 'EP08 딸기가 좋아! 딸기 종류 모음',
-    article_content: 'https://www.naver.com',
+    article_content:
+      'https://donghyun98.notion.site/EP08-609e8d11e290483f884d159a3ebd5f21?pvs=4',
     article_thumbnail_url:
       'https://res.cloudinary.com/dolc0qkxk/image/upload/v1699585042/008_ojpfxk.png',
   },
