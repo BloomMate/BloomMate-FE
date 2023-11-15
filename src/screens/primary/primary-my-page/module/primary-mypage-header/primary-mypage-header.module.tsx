f8830cd48220cd271b05417af381308fe1bdd1fd
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2023-11-15 18:18:28 +0900

    fix/마이페이지 헤더 수정

--- src/screens/primary/primary-my-page/module/primary-mypage-header/primary-mypage-header.module.tsx
+++ src/screens/primary/primary-my-page/module/primary-mypage-header/primary-mypage-header.module.tsx
@@ -1,16 +1,34 @@
 import { Stack } from '@mobily/stacks';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 
-import { Text } from '@/atoms';
+import {
+  PrimaryNavigatorParamLists,
+  PrimaryNavigatorProps,
+} from '../../../primary.navigator';
+
+import { ModalHeader } from '@/layouts/modal-header/modal-header.layout';
 
 type PrimaryMyPageHeaderModuleProps = {};
 
+export type PrimaryMypageScreenNavigatorProp = CompositeNavigationProp<
+  PrimaryNavigatorProps,
+  BottomTabNavigationProp<PrimaryNavigatorParamLists, 'PrimaryPlantListScreen'>
+>;
+
 export const PrimaryMyPageHeaderModule =
   ({}: PrimaryMyPageHeaderModuleProps) => {
+    const navigation = useNavigation<PrimaryMypageScreenNavigatorProp>();
+
     return (
       <Stack>
-        <Text fontWeight="Medium" variants="titleLarge" color="gray-900">
-          마이 페이지
-        </Text>
+        <ModalHeader
+          left={{ type: 'string', title: '마이페이지' }}
+          onPressExit={() => navigation.goBack()}
+        />
       </Stack>
     );
   };
