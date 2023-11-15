2c35c773e41260ed61cb81dd252df996c72017a1
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2023-11-15 18:11:15 +0900

    primary bold & 회원정보

--- src/screens/user-info/module/user-info-screen-content/user-info-screen-content.tsx
+++ src/screens/user-info/module/user-info-screen-content/user-info-screen-content.tsx
@@ -12,20 +12,28 @@ export const UserInfoContentModule = ({}: UserInfoContentModuleProps) => {
   const { user_name, account_id, tiiun_number, garden_size, address } = data;
 
   return (
-    <Stack
-      style={{
-        backgroundColor: palette['white'],
-        paddingHorizontal: 24,
-        paddingVertical: 24,
-        elevation: 4,
-        borderRadius: 8,
-      }}>
-      <Stack space={16}>
-        <UserInfoItem title="이름" content={user_name} />
-        <UserInfoItem title="계정 ID" content={account_id} />
-        <UserInfoItem title="Tiiun 번호" content={tiiun_number} />
-        <UserInfoItem title="정원 크기" content={String(garden_size)} />
-        <UserInfoItem title="주소" content={address} />
+    <Stack>
+      <Stack
+        style={{
+          backgroundColor: palette['white'],
+          paddingHorizontal: 24,
+          paddingVertical: 20,
+          elevation: 4,
+          borderRadius: 8,
+        }}>
+        <Stack paddingBottom={24}>
+          <Text variants={'titleLarge'} fontWeight={'Bold'} color={'gray-900'}>
+            회원 정보
+          </Text>
+        </Stack>
+
+        <Stack space={20}>
+          <UserInfoItem title="이름" content={user_name} />
+          <UserInfoItem title="계정 ID" content={account_id} />
+          <UserInfoItem title="Tiiun 제품키" content={tiiun_number} />
+          <UserInfoItem title="Tiiun 사이즈" content={String(garden_size)} />
+          <UserInfoItem title="주소" content={address} />
+        </Stack>
       </Stack>
     </Stack>
   );
@@ -40,11 +48,11 @@ const UserInfoItem: React.FC<UserInfoItemProps> = ({ title, content }) => (
   <Stack>
     <Stack horizontal>
       <Text
-        style={{ minWidth: 95 }}
+        style={{ minWidth: 120 }}
         variants={'bodyLarge'}
-        fontWeight={'Bold'}
+        fontWeight={'Medium'}
         color={'gray-900'}>
-        {title}:
+        {title}
       </Text>
       <Text variants={'bodyLarge'} fontWeight={'Medium'} color={'gray-900'}>
         {content}
--- src/screens/user-info/user-info.screen.tsx
+++ src/screens/user-info/user-info.screen.tsx
@@ -7,7 +7,6 @@ import { RootStackParamList } from '../root.navigator';
 import { UserInfoContentModule } from './module/user-info-screen-content';
 import { UserInfoHeaderModule } from './module/user-info-screen-header/user-info-screen-header';
 
-import { Text } from '@/atoms';
 import { BasicLayout } from '@/layouts';
 
 type UserInfoScreenProps = {};
@@ -25,21 +24,10 @@ export type UserInfoScreenNavigationRouteProps = RouteProp<
 export const UserInfoScreen = ({}: UserInfoScreenProps) => {
   return (
     <BasicLayout backgroundColor="gray-100">
-      <Stack space={76}>
+      <Stack space={32}>
         <UserInfoHeaderModule />
         <UserInfoContentModule />
       </Stack>
     </BasicLayout>
   );
 };
-
-interface UserInfoItemProps {
-  title: string;
-  content: string;
-}
-
-const UserInfoItem: React.FC<UserInfoItemProps> = ({ title, content }) => (
-  <Text variants={'bodyMedium'} fontWeight={'Medium'} color={'gray-900'}>
-    {title}: {content}
-  </Text>
-);
