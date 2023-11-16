d02d5ea122ca41c2ef2a8a8d5bca15ef570a26d7
hyuna <hyunah0923@hanyang.ac.kr>
2023-11-16 14:37:59 +0900

    feat: 웹뷰에서 뒤로갈때 커뮤니티로 돌아가게

--- src/screens/primary/primary-community/screen/primary-community-article/primary-article-webview.screen.tsx
+++ src/screens/primary/primary-community/screen/primary-community-article/primary-article-webview.screen.tsx
@@ -1,9 +1,11 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import { BackHandler } from 'react-native';
 import WebView from 'react-native-webview';
 
 import { RootStackParamList } from '../../../../root.navigator';
+import { PrimaryNavigatorProps } from '../../../primary.navigator';
 
 import { LoadingPage } from '@/layouts';
 
@@ -24,6 +26,17 @@ export const PrimaryArticleWebviewScreen =
     const {
       params: { article_content },
     } = useRoute<PrimaryArticleWebviewScreenRouteProps>();
+
+    const navigation = useNavigation<PrimaryNavigatorProps>();
+
+    useEffect(() => {
+      BackHandler.addEventListener('hardwareBackPress', () => {
+        navigation.replace('PrimaryStack', {
+          screen: 'PrimaryCommunityScreen',
+        });
+        return true;
+      });
+    }, []);
 
     return (
       <Suspense fallback={<LoadingPage />}>
