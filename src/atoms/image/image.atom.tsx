939582213b342dc9f38bea1541f419c21617248c
DongHyunKim <akainoo123@gmail.com>
2023-11-22 12:47:31 +0900

    fix : fix the fast-image

--- src/atoms/image/image.atom.tsx
+++ src/atoms/image/image.atom.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import FastImage, { FastImageProps } from 'react-native-fast-image';
 
-export type ImageProps = FastImageProps & {};
+import { Skeleton } from '../skeleton';
 
-export const Image = ({ ...props }: ImageProps) => {
-  return <FastImage {...props} />;
+export type ImageProps = FastImageProps & {
+  skeletonStyle?: {
+    width?: number | string;
+    height?: number | string;
+    borderRadius?: number;
+  };
+};
+
+export const Image = ({ skeletonStyle, ...props }: ImageProps) => {
+  const [isLoadingImg, setIsLoadingImg] = useState(true);
+
+  return (
+    <>
+      {isLoadingImg && (
+        <Skeleton
+          style={{ ...skeletonStyle, position: 'absolute', elevation: -1 }}
+        />
+      )}
+      <FastImage {...props} onLoadEnd={() => setIsLoadingImg(false)} />
+    </>
+  );
 };
--- src/screens/plant-add/modules/plant-add-content/components/plant-add-content-photo-complete/plant-add-content-complete.component.tsx
+++ src/screens/plant-add/modules/plant-add-content/components/plant-add-content-photo-complete/plant-add-content-complete.component.tsx
@@ -1,9 +1,10 @@
 import { memo } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
-import { Image } from 'react-native';
 
 import { PlantAddForm } from '../../../../hooks';
 import { EPlantAddStep } from '../../../../plant-add.state';
+
+import { Image } from '@/atoms';
 
 type PlantAddContentPhotoCompleteComponentProps = {};
 
@@ -18,6 +19,7 @@ export const PlantAddContentPhotoCompleteComponent =
 
     return (
       <Image
+        skeletonStyle={{ height: 200, width: '100%', borderRadius: 8 }}
         source={{ uri: field.value }}
         style={{ height: 200, borderRadius: 8 }}
       />
