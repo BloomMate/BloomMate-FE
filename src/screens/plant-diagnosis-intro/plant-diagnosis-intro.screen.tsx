27db2b3cde5fdf39aea4741530420f8e7b0752cd
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2023-11-30 17:30:06 +0900

    fix/진단하기 인트로 스크롤뷰

--- src/screens/plant-diagnosis-intro/plant-diagnosis-intro.screen.tsx
+++ src/screens/plant-diagnosis-intro/plant-diagnosis-intro.screen.tsx
@@ -13,7 +13,7 @@ import {
 } from './modules';
 
 import { PointLinearGradient } from '@/atoms';
-import { BasicLayout } from '@/layouts';
+import { BasicLayout, ScrollView } from '@/layouts';
 import { palette } from '@/utils';
 
 type PlantDiagnosisIntroScreenProps = {};
@@ -34,19 +34,21 @@ export const PlantDiagnosisIntroScreen =
       <PointLinearGradient style={{ width: '100%', height: '100%' }}>
         <BasicLayout backgroundColor="transparent">
           <PlantDiagnosisIntroHeaderModule />
-          <Suspense
-            fallback={
-              <Box
-                flex="fluid"
-                style={{ backgroundColor: palette['transparent'] }}
-                alignX="center"
-                alignY="center">
-                <ActivityIndicator animating={true} size="large" />
-              </Box>
-            }>
-            <PlantDiagnosisIntroContentModule />
-            <PlantDiagnosisIntroFooterModule />
-          </Suspense>
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <Suspense
+              fallback={
+                <Box
+                  flex="fluid"
+                  style={{ backgroundColor: palette['transparent'] }}
+                  alignX="center"
+                  alignY="center">
+                  <ActivityIndicator animating={true} size="large" />
+                </Box>
+              }>
+              <PlantDiagnosisIntroContentModule />
+              <PlantDiagnosisIntroFooterModule />
+            </Suspense>
+          </ScrollView>
         </BasicLayout>
       </PointLinearGradient>
     );
